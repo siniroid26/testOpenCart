f2609ffc0f96fc5ccd562aa7b611d1625713c79f
Aleksandr Nikolchenko <75898267+siniroid26@users.noreply.github.com>
2023-11-08 01:05:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
+from flask import Flask, jsonify
 import requests
-def get_external_ip():
-    response = requests.get("http://ipinfo.io/json")
-    data = response.json()
-    return data.get('ip')
 
-external_ip = get_external_ip()
-print(f"Ваш внешний IP-адрес: {external_ip}")
-import json
+app = Flask(__name__)
 
 # Ваши данные для API
 api_name = 'aspor32'
 api_key = 'VqWUxWnOH223DPB1TR1sqnj6wLJofsHQvxjYmr8lJ7FS3torc51Nj0b2msAK3doaNbCXB8GQtuK2Fr31kbVX7BBtkyC68EoSzCb78Z80jZkABuKHAUtHKNL44yFSUwSmRuzYKG4K8pCFXzStRyuHxTteaw4ggpqUcPEOXjNasNUqp6NxLoTsoNFsfKkc3BOKVsRO5Ip8hNPUFquklnCZrDMKxFyKSWVRSBU48Q0PZ7dB7OnzCzYGN82AuBZOcDsq'
 # URL API
-url = 'https://test.aspor.ua/api/rest/categories'
+url = 'https://test.aspor.ua'
 
-# Заголовки для запроса
-headers = {
-    'Content-Type': 'application/json',
-    'Api-Name': api_name,
-    'Api-Key': api_key
-}
+@app.route('/products', methods=['GET'])
+def get_products():
+    response = requests.get(f'{url}/api/products', headers={'api_name': api_name, 'api_key': api_key})
+    products = response.json()
+    return jsonify(products)
 
-# Отправляем запрос
-response = requests.get(url, headers=headers)
-
-# Проверяем статус ответа
-if response.status_code == 200:
-    # Преобразуем ответ в JSON
-    data = response.json()
-
-    # Выводим категории товаров
-    for category in data['data']:
-        print(category['name'])
-else:
-    print(f'Ошибка подключения: {response.status_code}')
+if __name__ == '__main__':
+    app.run(debug=True)
